67310e31d15e99febff8212d20072d27f241c181
Auric01 <vare.140201@gmail.com>
2022-08-04 22:06:01 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿bool ctrl = true;
+
+do {
+    Console.Write("En que número estás pensando entre 0 y 1000?");
+    string? respuesta = Console.ReadLine();
+
+    if( respuesta?.ToUpper() == "FINALIZAR") {
+    
+        ctrl = false;
+    
+    } else {
+        
+        try
+        {
+
+            int num = int.Parse(respuesta ?? string.Empty);
+
+            if (num <= 1000 && num >= 0){
+
+                if ( (num % 2) == 0 || num == 0 ) {
+
+                    Console.WriteLine("¡Es un número par! ¿puedes añadir otro?\n");
+
+                } else {
+
+                    Console.WriteLine("¡Es un número impar! ¿puedes añadir otro?\n");
+
+                }
+
+            } else {
+
+                Console.WriteLine("Numero fuera de rango");
+
+            }
+             
+        }
+        catch (System.FormatException)
+        {
+
+            Console.WriteLine("No has ingresado un respuesta valida");
+
+        }
+
+    }
+
+} while (ctrl);
+
+Console.WriteLine("Ha finalizado el programa");
